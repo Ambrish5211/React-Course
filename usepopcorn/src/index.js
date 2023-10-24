ee3e9e3b903e5b0fdafe0f7aeef17eff00872756
Ambrish <ambrish19.5211@gmail.com>
2023-10-25 01:43:45 +0530

    done with star rating api component

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -7,7 +7,7 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={10} />
+    <StarRating maxRating={5} className='test' message= {["terrible", "okay", "good", "excellent", "outstanding"]} defaultRating={0} />
   </React.StrictMode>
 );
 
