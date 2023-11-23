8e73111b5440d8c9ed6908502b75ed974667fa59
Ambrish <ambrish19.5211@gmail.com>
2023-11-24 02:52:26 +0530

    added 3 pages and corresponding components

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,6 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Product from "./pages/Pricing";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
-    <div>WorldWise</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="product" element={<Product/>} />
+        <Route path="pricing" element={<Pricing/>} />
+        <Route path="login" element={<Login/>} />
+        
+        <Route path="*" element={<PageNotFound/>} />
+        <Route path="app" element={<AppLayout/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
