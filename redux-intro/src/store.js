ecdd35a3efb9fa269a9ae48fb03c659698c9af42
Ambrish <ambrish19.5211@gmail.com>
2023-12-07 20:16:31 +0530

    levaraging store

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,37 +1,12 @@
-import { combineReducers, createStore } from 'redux';
-import accountReducer, { deposit, payLoan, requestLoan, withdraw } from './features/accounts/accountSlice';
-import customerReducer, { createCustomer } from './features/customer/customerSlice';
-
-
-
-
-
-
-
-
-
-
-
-
-
+import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customer/customerSlice";
 
 const rootReducer = combineReducers({
   accountStore: accountReducer,
-  customerStore: customerReducer
-})
-const store = createStore(rootReducer)
+  customerStore: customerReducer,
+});
+const store = createStore(rootReducer);
 
-store.dispatch(deposit(500))
-store.dispatch(withdraw(200))
-store.dispatch(requestLoan(1000, "Buy a star"))
-store.dispatch(payLoan())
-console.log(store.getState());
+export default store;
 
-
-
-
-
-
-
-store.dispatch(createCustomer("Ambrish Kumar", "addharcard"))
-console.log(store.getState())
