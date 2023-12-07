13d4d1d57d99340e00df8f84eee4f133f9d2c6ad
Ambrish <ambrish19.5211@gmail.com>
2023-12-08 04:33:03 +0530

    implemented balance page

--- redux-intro/src/App.js
+++ redux-intro/src/App.js
@@ -2,15 +2,22 @@ import CreateCustomer from "./features/customer/CreateCustomer";
 import Customer from "./features/customer/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((state) => state.customerStore.fullName)
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {fullName === "" ? (<CreateCustomer />) : (
+        <>
+        <Customer />
+        <AccountOperations />
+        <BalanceDisplay />
+        </>
+      )}
+      
+      
     </div>
   );
 }
