0bba8cd44c69e45f8040d2bad331b8588f617879
Ambrish <ambrish19.5211@gmail.com>
2023-12-09 00:29:59 +0530

    setting up the project

---
+++ fast-react-pizza/features/order/OrderItem.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { formatCurrency } from "../../utils/helpers";
+
+// eslint-disable-next-line no-unused-vars
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  // eslint-disable-next-line react/prop-types
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li>
+      <div>
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
