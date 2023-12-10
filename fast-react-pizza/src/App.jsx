c156de63da50b64f6223fb057485cb232e93184f
Ambrish <ambrish19.5211@gmail.com>
2023-12-11 02:30:53 +0530

    adding errror component for unknow requests

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -4,11 +4,13 @@ import Menu , { loader as menuLoader }from "../features/menu/Menu"
 import Cart from "../features/cart/Cart"
 import CreateOrder from "../features/order/CreateOrder"
 import Order from "../features/order/Order"
+import Error from "./ui/Error"
 import AppLayout from "../ui/AppLayout"
 
 const router = createBrowserRouter([
   {
     element: <AppLayout/>,
+    errorElement: <Error />,
     children: [{
       path: "/",
       element: <Home />,
@@ -16,6 +18,7 @@ const router = createBrowserRouter([
     {
       path: "/menu",
       element: <Menu />,
+      errorElement: <Error />,
       loader: menuLoader
     },
     {
